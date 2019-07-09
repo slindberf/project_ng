147ad3a7c0212da19b997dac8ebddb90820fa5b8
sara li fo <saralfontess@gmail.com>
2019-07-09 17:51:03 +0200

    2 way binding
    import FormsMosule to use ngModule

--- my-project/src/app/app.module.ts
+++ my-project/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { from } from 'rxjs';
 
 @NgModule({ //identify the class as an angular module by attaching the ng module Decorator
   //and passing in metadata defining the details of this angular model
@@ -11,7 +12,8 @@ import { ProductListComponent } from './products/product-list.component';
     ProductListComponent 
   ], //in the declarations array we define which of our components belong to this module
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ], //in the imports array we define the external modules that we want to have available to all of the components that belong to this angular module  
   //BrowserModule which every browser app must import. BrowserModule registers important application service providers such as error handling 
   bootstrap: [AppComponent] //defines the startup component of our app
