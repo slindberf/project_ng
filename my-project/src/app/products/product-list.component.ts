8b28a2ab3e76b959a4691073935d9e01ced56d25
sara li fo <saralfontess@gmail.com>
2019-07-09 17:51:03 +0200

    2 way binding
    import FormsModule to use ngModule with an input

--- my-project/src/app/products/product-list.component.ts
+++ my-project/src/app/products/product-list.component.ts
@@ -10,6 +10,7 @@ export class ProductListComponent {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false; // so the images are not displayed when the page is first loaded
+    listFilter: string = 'cart';
     products: any[] = [
         {
           "productId": 1,
