de9cff4fbedb78efffb1139725f3bc2f64dea4e2
sara li fo <saralfontess@gmail.com>
2019-07-10 12:34:29 +0200

    service ProductService
    @Injectable

--- my-project/src/app/products/product-list.component.ts
+++ my-project/src/app/products/product-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service"; 
 
 @Component({ //it is the Decorator that makes this class a component
     selector: 'pm-products',
     templateUrl: './product-list.component.html', //to find our view
-    styleUrls: ['./product-list.component.css']
+    styleUrls: ['./product-list.component.css'],
+    //providers: [ProductService] -> already registered
 }) //component decorator that defines our metadata
 
 export class ProductListComponent implements OnInit{
@@ -29,42 +31,10 @@ export class ProductListComponent implements OnInit{
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2016",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.2,
-          "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2016",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-          "productId": 3,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2016",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-      }
-    ]; //when we don't know the data type we use an array of any
+    products: IProduct[] = []; //when we don't know the data type we use an array of any
     
-    constructor(){
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart'; 
+    constructor(private productService: ProductService){ //executed when the component is created
+
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -78,8 +48,10 @@ export class ProductListComponent implements OnInit{
         this.showImage = !this.showImage;
         //in the template show image only if true: <img *ngIf='showImage' >
     }
-    ngOnInit(): void {
-        console.log("In OnInit");
+    ngOnInit(): void { //perform any component initialization
+        this.products = this.productService.getProducts();  
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart'; 
     }
 } //export to be available to other parts of the app
 //our class which finds our associated code
