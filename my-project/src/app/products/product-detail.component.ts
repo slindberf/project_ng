985715bbd2392c624841a52c70adb2a7a8262ab4
sara li fo <saralfontess@gmail.com>
2019-07-11 10:44:48 +0200

    product detail snapshot

--- my-project/src/app/products/product-detail.component.ts
+++ my-project/src/app/products/product-detail.component.ts
@@ -15,7 +15,18 @@ export class ProductDetailComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+        "productId": id,
+        "productName": "Leaf Rake",
+        "productCode": "GDN-0011",
+        "releaseDate": "March 19, 2016",
+        "description": "Leaf rake with 48-inch wooden handle.",
+        "price": 19.95,
+        "starRating": 3.2,
+        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png" 
+    }
   }
 
 }
