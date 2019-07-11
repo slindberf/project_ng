394eb016b969340904da678eb189f2dbab3f909e
sara li fo <saralfontess@gmail.com>
2019-07-11 12:04:59 +0200

    Product Detail Page

--- my-project/src/app/products/product-detail.component.ts
+++ my-project/src/app/products/product-detail.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IProduct } from './product';
-import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'Product Detail';
-  product: IProduct;
+  pageTitle = 'Product Detail';
+  errorMessage = '';
+  product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute) { 
-    console.log(this.route.snapshot.paramMap.get('id'));
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService) {
   }
 
   ngOnInit() {
-    let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += `: ${id}`;
-    this.product = {
-        "productId": id,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png" 
+    const param = this.route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getProduct(id);
     }
+  }
+
+  getProduct(id: number) {
+    this.productService.getProduct(id).subscribe(
+      product => this.product = product,
+      error => this.errorMessage = <any>error);
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
